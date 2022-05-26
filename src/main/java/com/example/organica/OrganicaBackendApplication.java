c3dfc031af09be6f4a9a8cfecd24066a4dbf1af4
NguyenTrung-PM <96853789+NguyenTrung-PM@users.noreply.github.com>
2022-05-26 11:42:31 +0700

    init

---
+++ src/main/java/com/example/organica/OrganicaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.organica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganicaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/organica/OrganicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.organica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
